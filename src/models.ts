d36ccd8ed2b3339ac617d1a2b8ef9df34ed2ec92
Walfie <walfington@gmail.com>
2018-07-29 20:05:52 -0400

    Add character search

--- src/models.ts
+++ src/models.ts
@@ -1,6 +1,6 @@
 export type MediaId = string;
 
-export type MediaType = "anime" | "manga";
+export type MediaType = "anime" | "manga" | "character";
 
 export interface Media {
   id: MediaId;
--- src/search.ts
+++ src/search.ts
@@ -6,8 +6,8 @@ export interface Search {
 
 export class AniListSearch implements Search {
   private baseUrl = "https://graphql.anilist.co";
-  private query: string = `
-   query($page: Int = 1, $perPage: Int = 10, $search: String, $type: MediaType, $sort: [MediaSort]) {
+  private mediaQuery: string = `
+    query($page: Int = 1, $perPage: Int = 10, $search: String, $type: MediaType, $sort: [MediaSort]) {
       page: Page(page: $page, perPage: $perPage) {
         media: media(search: $search, sort: $sort, type: $type) {
           id
@@ -17,20 +17,34 @@ export class AniListSearch implements Search {
       }
     }`;
 
+  private characterQuery: string = `
+    query ($page: Int = 1, $perPage: Int = 10, $search: String, $sort: [CharacterSort]) {
+      page: Page(page: $page, perPage: $perPage) {
+        characters: characters(search: $search, sort: $sort) {
+          id
+          name { first last }
+          image { large }
+        }
+      }
+    }`;
+
   public async searchMedia(
     query: string,
     mediaType: MediaType
   ): Promise<[Media]> {
-    const body: any = {
-      query: this.query,
-      variables: {
-        page: 1,
-        perPage: 10,
-        search: query,
-        type: mediaType.toUpperCase(),
-        sort: ["POPULARITY_DESC", "SEARCH_MATCH"]
-      }
-    };
+    switch (mediaType) {
+      case "anime":
+      case "manga":
+        return this.searchAnimeOrManga(query, mediaType);
+        break;
+      case "character":
+        return this.searchCharacter(query);
+        break;
+    }
+  }
+
+  private async searchJson(query: string, variables: any): Promise<any> {
+    const body: any = { query, variables };
 
     const result = await fetch(this.baseUrl, {
       method: "POST",
@@ -41,7 +55,41 @@ export class AniListSearch implements Search {
       body: JSON.stringify(body)
     });
 
-    const json = await result.json();
+    return await result.json();
+  }
+
+  private async searchCharacter(query: string): Promise<[Media]> {
+    let variables = {
+      page: 1,
+      perPage: 10,
+      search: query,
+      sort: ["SEARCH_MATCH", "FAVOURITES_DESC"]
+    };
+
+    const json = await this.searchJson(this.characterQuery, variables);
+
+    return json.data.page.characters.map(character => ({
+      id: "character:" + character.id,
+      title: [character.name.last, character.name.first]
+        .filter(Boolean)
+        .join(" "),
+      image: character.image.large
+    }));
+  }
+
+  private async searchAnimeOrManga(
+    query: string,
+    mediaType: MediaType
+  ): Promise<[Media]> {
+    let variables = {
+      page: 1,
+      perPage: 10,
+      search: query,
+      type: mediaType.toUpperCase(),
+      sort: ["POPULARITY_DESC", "SEARCH_MATCH"]
+    };
+
+    const json = await this.searchJson(this.mediaQuery, variables);
 
     return json.data.page.media.map(media => ({
       id: mediaType + ":" + media.id,
